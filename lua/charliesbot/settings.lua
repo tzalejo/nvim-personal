958f51a5b38a5bc81ce0001aec7f9065bf44ac8e
Alejandro Valenzuela <tzalejo@gmail.com>
2023-03-17 21:15:42 -0300

    modifico nro de tabulacion

--- lua/charliesbot/settings.lua
+++ lua/charliesbot/settings.lua
@@ -11,9 +11,9 @@ vim.opt.signcolumn = 'yes'
 
 -- Indentation & tabs
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
